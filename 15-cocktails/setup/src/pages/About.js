292444143e934d25d9bd9d1f07cdc0bc405bd126
jordan huang <jordanhuang99@gmail.com>
2020-12-19 01:01:40 +0100

    15-cocktails completed

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Commodi adipisci ipsam, explicabo error dicta eum consequuntur 
+        minima numquam sequi natus quibusdam iusto a, dolores consequatur 
+        sint dolorem rem minus eveniet corporis fuga enim? Sapiente iure iusto 
+        nisi ab. Veniam, quidem.
+      </p>
+    </section>
   )
 }
 
